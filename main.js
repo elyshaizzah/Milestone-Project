e556a260abf5d8514a55c815909a960aec2c6333
Elysha Izzah <strawberryelysha2005@gmail.com>
2019-05-18 11:42:11 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+function changequote(){
+
+const maxNum = "people.length" 
+const num = getRandomInt(maxNum)
+console.log(num)
+console.log(quotes[num])
+}
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+
+
+
+const quotes = [
+    {
+    quote:"yeehaw!",
+    author: "yeet"
+    },
+    {
+    quote:"yeet",
+    author:"shhwhdhdb"
+    },
+    {
+    quote:"hawyee",
+    author:"nhshswjsnn"
+    }    
+ ]
+ 
+
+    
+    // document.getElementById("quote1").innerHTML = "achieve the best!";
+
+
+    
